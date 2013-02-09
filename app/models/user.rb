9e68e225763ff3126f607c92f79225da942eb6e3
mohanraj-nagasamy <mohanraj.nagasamy@octanner.com>
2013-02-09 12:14:36 -0700

    finish User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,3 +23,13 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
 end
+
+class Person
+  include ActiveModel::Validations
+
+  attr_accessor :first_name, :last_name
+
+  validates_each :first_name, :last_name do |record, attr, value|
+    record.errors.add attr, 'starts with z.' if value.to_s[0] == zz
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -90,11 +90,18 @@ describe User do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
-
-      puts "USER ==> #{User.all}"
     end
-
     it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
   end
 
   describe "return value of authenticate method" do
